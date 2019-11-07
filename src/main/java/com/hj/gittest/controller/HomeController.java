55f734d89188c28509060383998f9906da89515e
hujing <23936987@qq.com>
2019-11-07 09:08:24 +0800

    first commit

---
+++ src/main/java/com/hj/gittest/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.hj.gittest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
